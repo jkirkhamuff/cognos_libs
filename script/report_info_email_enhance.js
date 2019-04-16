d5d6591eac813373e0d8381ff59b24f9e460cbc4
jkirkhamuff <49444679+jkirkhamuff@users.noreply.github.com>
2019-04-16 14:41:33 -0400

    Update report_info_email_enhance.js
    de-HTML-ify

--- script/report_info_email_enhance.js
+++ script/report_info_email_enhance.js
@@ -1,4 +1,4 @@
-<!--
+/*
 Author: 		Joe K
 Version Date:	2019/02/15 v1
 Description:
@@ -9,23 +9,19 @@ ASSUMPTIONS: 	1) There is an HTML object with a tag directly to the *right* of t
 				and the tag's ID='reportNumber'.
 
 				2) There is a tag with ID='emailLink' on the page, and the tag can accept an 'href' parameter.
--->
+*/				
 
-<script>
+//grab user-defined report number di_Report_Number via custom HTML target
+var num = document.getElementById("reportNumber").previousElementSibling.innerText;
 
-	//grab user-defined report number di_Report_Number via custom HTML target
-	var num = document.getElementById("reportNumber").previousElementSibling.innerText;
-	
-	//grab the custom email link HTML object
-	var emailLink = document.getElementById("emailLink");
+//grab the custom email link HTML object
+var emailLink = document.getElementById("emailLink");
 
-	//define the href text
-	var text = ("mailto:DataHelp@uff.ufl.edu?subject=AP Report Support - Report # " + num) 
+//define the href text
+var text = ("mailto:DataHelp@uff.ufl.edu?subject=AP Report Support - Report # " + num) ;
 
-	//set the subject line
-	emailLink.href = (text);
+//set the subject line
+emailLink.href = (text);
 
-	//ensure that the actual text rendered to the user is the email address
-	emailLink.innerText = "DataHelp@uff.ufl.edu"
-
-</script>
+//ensure that the actual text rendered to the user is the email address
+emailLink.innerText = "DataHelp@uff.ufl.edu";
